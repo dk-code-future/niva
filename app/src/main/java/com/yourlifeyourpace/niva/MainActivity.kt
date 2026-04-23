aa9d40e79b70b5133b587252be9d4d4718bc67f6
Deepthi Kantipudi <dkantipudi+sfemu@salesforce.com>
2026-04-23 14:40:41 -0400

    Day 1 : Hello Niva

--- app/src/main/java/com/yourlifeyourpace/niva/MainActivity.kt
+++ app/src/main/java/com/yourlifeyourpace/niva/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             NivaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Niva",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello $name.\nYour life, your pace.",
         modifier = modifier
     )
 }
