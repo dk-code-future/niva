a826a3ac939ea6e414cb1acebdc663efbc39b454
Deepthi Kantipudi <dkantipudi+sfemu@salesforce.com>
2026-04-24 11:38:24 -0400

    Day 2: Niva running on real device, dev loop confirmed

--- app/src/main/java/com/yourlifeyourpace/niva/MainActivity.kt
+++ app/src/main/java/com/yourlifeyourpace/niva/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name.\nYour life, your pace.",
+        text = "Hello $name.\nYour life, your pace. ",
         modifier = modifier
     )
 }
